95a844e2436eda486b8340411490cb2cb405b474
pigzzz123 <205270006@qq.com>
2022-01-22 01:52:23 +0800

    优化域名模板

--- resources/views/themes/default/views/domains/show.blade.php
+++ resources/views/themes/default/views/domains/show.blade.php
@@ -128,7 +128,7 @@
                                         </div>
                                         <div class="space-y-1">
                                             <div class="relative">
-                                                <input type="text" x-model="form.phone" class="w-full block placeholder-gray-400 border border-gray-200 rounded pl-12 pr-5 py-3 leading-6 text-lg focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" placeholder="{{ lang('domain.offer_phone_placeholder') }}">
+                                                <input type="text" x-model="form.phone" class="w-full block placeholder-gray-400 border border-gray-200 rounded pl-12 pr-5 py-3 leading-6 text-lg focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" placeholder="+229 69488999">
                                                 <div class="absolute left-0 inset-y-0 w-12 my-px ml-px flex items-center justify-center text-gray-500">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                                                         <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
